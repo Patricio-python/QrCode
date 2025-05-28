18e7b2426c842cf4b4e082ba89387d8e6f871774
Luiz Patricio <patricio.python@gmail.com>
2025-05-28 20:21:04 -0300

    Update QrCreator.py
    Adicionado um banner, e melhorias do código.

--- QrCreator.py
+++ QrCreator.py
@@ -1,5 +1,12 @@
 import segno, segno.helpers
-
+import pyfiglet
+#Banner do Programa
+banner = pyfiglet.figlet_format("QrCreator", font = "slant") 
+print(banner) 
+#Função para o usuario selecionar o tipo de QrCode que deseja criar
+def opcao():
+    global tipo
+    tipo=int(input("\n1:Wifi\n2:Site\n3:Finalizar o programa\n\nOpção: "))
 #Função para criar QrCode para um website
 def site():
     #O usuário deve entrar com a Url do web site
@@ -10,7 +17,9 @@ def site():
     #ou os blocos (campo "Dark") e o tamanho (campo "scale")
     QrSite = segno.make(site)
     QrSite.save(nome+'.png', dark="black", light="white", scale=10)
-
+    #Retorno para escolher outras opções
+    print("\nQrCode criado na pasta do programa\nSe quiser continuar use uma das opções. (1 , 2 ou 3)")
+    opcao()
 #Função para criar QrCode para uma rede Wifi
 def wifi():
     #O usuário deve fornecer o nome da rede wifi
@@ -31,39 +40,25 @@ def wifi():
     #O segno cria o qr code com as informações dada, podendo mudar as cores do fundo (campo "light")
     #ou os blocos (campo "Dark") e o tamanho (campo "scale")
     wifi.save(nome+".png", dark="black", light="white", scale=10)
+    #Retorno para escolher outras opções
+    print("\nQrCode criado na pasta do programa\nSe quiser continue use uma das opções. (1 , 2 ou 3)")
+    opcao()
 #O usuário deve digitar uma das opções
 print("Bem vindo! Para começar selecione opção de QrCode desejas criar. (1 ,2 ou 3)")
-tipo=int(input("\n1:Wifi"
-        "\n2:Site"
-        "\n3:Finalizar o programa\n"
-        "\nOpção: "))
+opcao()
 #Loop de repetição para o programa continuar rodando enquanto não for selecionado a opção 3
 while tipo!=3:
     #Se a opção selecionada for 1 a função wifi sera usada
     if (tipo==1):
         wifi()
-        #O usuário poderá escolher outra opção ou finalizar o programa
-        print("QrCode criado na pasta do programa\n"
-        "\nSe quiser continue use uma das opções. (1 , 2 ou 3)")
-        tipo=int(input("\n1:Wifi"
-        "\n2:Site"
-        "\n3:Finalizar o programa\n"
-        "\nOpção: "))
+
     elif tipo==2:
         #Se a opção selecionada for 2 a função site sera usada
         site()
-        #O usuário poderá escolher outra opção ou finalizar o programa
-        print("QrCode criado na pasta do programa"
-        "\nSe quiser continuar use uma das opções. (1 , 2 ou 3)")
-        tipo=int(input("\n1:Wifi"
-        "\n2:Site"
-        "\n3:Finalizar o programa\n"
-        "\nOpção: "))
+                
     else:
         #No caso de uma opção diferente o usuário terá uma informação de que foi inválido a seleção
         #Nesse caso ele poderá selecionar outra opção
         print("Opção inválida! Use uma das opções abaixo. (1, 2 ou 3)")
-        tipo=int(input("\n1:Wifi"
-        "\n2:Site"
-        "\n3:Finalizar o programa\n"
-        "\nOpção: "))
+        opcao()
+print("Obrigado por usar o QrCreator")
